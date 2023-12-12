ba55ee62b55fef576e1082932a7c300fa25f37e0
DylanDinesh <350444048@gapps.yrdsb.ca>
2023-12-11 21:32:18 -0500

    Update With Feature

--- MyWorld.java
+++ MyWorld.java
@@ -8,34 +8,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World {
     private int score = 0; // The player's score
-    private int applesMissed = 0; // The count of apples missed by the player
+    private int aliensMissed = 0; // The count of aliens missed by the player
 
     // Constructor for MyWorld. It sets up the initial game state.
     public MyWorld() {    
         super(600, 400, 1); // Create a new world with 600x400 cells, each cell being 1x1 pixels
         prepare();
     }
-    
+
     // Prepare the world by adding initial objects.
     private void prepare() {
-        Elephant elephant = new Elephant();
-        addObject(elephant, getWidth() / 2, getHeight() - elephant.getImage().getHeight() / 2);
+        Spaceship spaceship = new Spaceship();
+        addObject(spaceship, getWidth() / 2, getHeight() - spaceship.getImage().getHeight() / 2);
     }
 
     // The act method is called repeatedly to refresh the game state.
     public void act() {
-        if (Greenfoot.getRandomNumber(100) < 2) { // There's a 2% chance to add a new apple each act cycle
-            addObject(new Apple(), Greenfoot.getRandomNumber(getWidth()), 0); // Add an apple at a random x-coordinate at the top of the world
+        if (Greenfoot.getRandomNumber(100) < 2) { // There's a 2% chance to add a new alien each act cycle
+            addObject(new Alien(), Greenfoot.getRandomNumber(getWidth()), 0); // Add an alien at a random x-coordinate at the top of the world
         }
 
-        // Use showText to display the score and missed apples count
-        showText("Score: " + score + " | Missed: " + applesMissed, 300, 20);
+        // Use showText to display the score and missed aliens count
+        showText("Score: " + score + " | Missed: " + aliensMissed, 300, 20);
 
         // Check for win and lose conditions
         if (score >= 66) { // Winning condition
             showEndMessage("You Win!");
             Greenfoot.stop();
-        } else if (applesMissed >= 6) { // Losing condition
+        } else if (aliensMissed >= 6) { // Losing condition
             showEndMessage("Game Over!");
             Greenfoot.stop();
         }
@@ -46,9 +46,9 @@ public class MyWorld extends World {
         score++;
     }
 
-    // Method to increment the count of missed apples
-    public void appleMissed() {
-        applesMissed++;
+    // Method to increment the count of missed aliens
+    public void alienMissed() {
+        aliensMissed++;
     }
 
     // Method to display the end game message
